dd88337f9a9b80cde7ba4ff5631f4dfa915a2760
Pe-ter-M <peterwahu3491@gmail.com>
2025-11-20 16:20:23 +0300

    post

--- learn.py
+++ learn.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Body, HTTPException,status
 
 
 app = FastAPI()
+
+users ={ 
+   1: {"name":"peter"},
+    2:{"name":"Jane"}
+}
 
 @app.get("/")
 async def read_root():
     return {'Hello':"world"}
 
+@app.get("/users/{id}")
+async def get_user(id:int):
+    print(id)
+    user = users.get(id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'This id {id} not found')
+    return {'user':user}
+
 @app.post('/')
-async def receive_data():
+async def receive_data(payload: dict = Body()):
+    print(payload)
     return {'data received'}
