90cff61ae6174b207e4078c7e01676b8550dbc4a
Pe-ter-M <peterwahu3491@gmail.com>
2025-11-20 16:36:18 +0300

    variables

--- learn.py
+++ learn.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI,Body, HTTPException,status
+from pydantic import BaseModel
+from typing import Optional
+from enum import Enum
 
 
 app = FastAPI()
@@ -7,6 +10,15 @@ users ={
    1: {"name":"peter"},
     2:{"name":"Jane"}
 }
+class Role(str,Enum):
+    ADMIN = 'admin'
+    USER = 'user'
+    GUEST = 'guest'
+
+class User(BaseModel):
+    name:str
+    age: Optional[int]
+    role: Role = Role.USER
 
 @app.get("/")
 async def read_root():
@@ -21,6 +33,6 @@ async def get_user(id:int):
     return {'user':user}
 
 @app.post('/')
-async def receive_data(payload: dict = Body()):
+async def receive_data(payload: User):
     print(payload)
     return {'data received'}
